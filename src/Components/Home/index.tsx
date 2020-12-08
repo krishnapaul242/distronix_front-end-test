b20c935c05fea605158d9d912789ee8ac88c82ea
krishnapaul242 <krishnapaulmailbox@gmail.com>
2020-12-08 18:52:06 +0530

    Add some styles Fixes #9

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="Home-content">
+            <h1>Distronix: Front End Tests</h1>
+            <p>This test is developed for the fulfillment of front end test for Distronix. Please visit the github repository at <a href="https://github.com/krishnapaul242/distronix_front-end-test">https://github.com/krishnapaul242/distronix_front-end-test</a></p>
+        </div>
+    );
+}
+export default Home;
